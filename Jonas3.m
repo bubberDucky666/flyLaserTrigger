66ec3c06d30a398dda70aa5c5bcce2fc9a1e608e
Jonas Kolker <36770208+bubberDucky666@users.noreply.github.com>
2019-07-22 14:48:41 -0400

    Improvements and additions
    More accurate light flash control
    
    Added parameter for duration of light flash
    Added parameter for duration of light break
    Added parameter for strength of light (although doesn't work with non 0 val yet)
    Added parameter for program timeout
    
    Added some comments for clarity (things look a bit more confusing now)

--- Jonas3.m
+++ Jonas3.m
@@ -2,16 +2,22 @@
 function Jonas3 ()
     vidName = 'myVideo.avi';  
 
-    thr  = 80;
-    minA = 100;
-    maxA = 500000;
-    fps  = 15;
-    dur  = 10; %in seconds
+    thr  = 80;      %threshold (the lower, the less inclusive)
+    minA = 100;     %minimum area
+    maxA = 500000;  %maximum area
+    fps  = 15;      %frames per second
+    dur  = 10;      %duration of light flashign in seconds
+    tOut = 60;      %time before program gives up in seconds
+    lWt  = 5.0;     %how long before initial flash (after detection)
+    lBtw  = .5;     %how long between flashes
+    lDur = .5;      %how long flash lasts
+    lStr = 5;       %streangth of light (lower is stronger)
     
     global cut; 
     cut = true;
 
-    aviObject = VideoWriter(vidName);  % Create a new AVI file
+    aviObject           = VideoWriter(vidName);  % Create a new AVI file
+    aviObject.FrameRate = fps;
     open(aviObject);
     
     %aviObject.FrameRate = fps;
@@ -20,19 +26,23 @@ function Jonas3 ()
     vid.FramesPerTrigger = Inf;
 
        
-    check = true;
-    
+    check1 = true;   %starting image aquisition
+
     writeDigitalPin(ard, 'D5', 1);
     
-    start(vid);
     preview(vid);
     
-    b = tic;
-    e = toc(b);
+    %VIDEO ACQUISITION STARTS HERE AUTOMATICALLY. FOR A CONDITIONAL
+    %START, PUT IT RIGHT HERE :)
+    
+    start(vid);
+    
+    a = tic;
+    b = toc(a);
     
     
-    while e <= dur
-        im = getsnapshot(vid);
+    while b <= tOut
+        im  = getsnapshot(vid);
         im  = imresize(im, 0.33);
         
         tm  = imgaussfilt(im, 12);
@@ -42,26 +52,46 @@ function Jonas3 ()
         fIm = imresize(fIm, .5);
         props = regionprops(fIm, 'Area', 'Perimeter','PixelIdxList');
         
+        c = tic;     %baseline for timer d (when object is detected)
+        
         if size(props) > 0
-            if check
-                lOn = vid.FramesAcquired;
-                check = false;
-            end
-            disp('yus');
-
-            e = round(toc(b));
+            if check1
+                lOn    = vid.FramesAcquired;
+                check1 = false;
             
-            if mod(e, 5)
-                writeDigitalPin(ard, 'D5', 0); %lights on
             else
-                writeDigitalPin(ard, 'D5', 1);
+                while true              %fly is detected and rec started
+
+                    disp('yus');
+
+                    d = round(toc(c));  %checks timer since object was detected
+
+                    disp(d);
+                    if mod(d, lWt)   %initial wait time over
+                        disp('butt');
+                        while d < dur %once waitime over
+                            writeDigitalPin(ard, 'D5', lStr); %lights on
+                            disp('light on');
+                            pause(lDur);
+                            writeDigitalPin(ard, 'D5', 1);
+                            disp('light off');
+                            pause(lBtw)
+                            d = round(toc(c));   %checks timer since object was detected
+                        end
+                        break          %break out of while true loop
+                    
+                    elseif d < lWt   %initial wait time not reached
+                        disp('not yet'); 
+                    end
+                end
+                break    %break out of overall loop when vid is over
             end
-            
         else
-            writeDigitalPin(ard, 'D5', 1);
-            disp('nah');
+            b = toc(a);  %refreshes timer that times out program
         end
         
+        writeDigitalPin(ard, 'D5', 1);  %this is if nothing's been detected yet
+        disp('nah');
     end
     
     frames = getdata(vid);
